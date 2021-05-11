6bbf2a6fda0885a871a09dfdce493472149f892c
Rayhan Islam <67514865+Rayhan0Islam0Shagor@users.noreply.github.com>
2021-05-12 00:27:38 +0600

    update

--- src/pages/country/[id].js
+++ src/pages/country/[id].js
@@ -119,7 +119,7 @@ export default Country;
 
 export const getStaticPaths = async () => {
   const res = await fetch("https://restcountries.eu/rest/v2/all");
-  const countries = res.json();
+  const countries = await res.json();
 
   const paths = countries.map((country) => ({
     params: { id: country.alpha3Code },
@@ -127,7 +127,7 @@ export const getStaticPaths = async () => {
 
   return {
     paths,
-    fallbacks: false,
+    fallback: false,
   };
 };
 
