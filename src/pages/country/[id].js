b0a5e954cb5dafa511049877b7376a00295f5bf8
Rayhan Islam <67514865+Rayhan0Islam0Shagor@users.noreply.github.com>
2021-05-11 23:27:25 +0600

    going for diploy

---
+++ src/pages/country/[id].js
@@ -0,0 +1,128 @@
+import { useEffect, useState } from "react";
+import Layout from "../../components/Layout/Layout";
+
+import styles from "./Country.module.css";
+
+const getCountry = async (id) => {
+  const res = await fetch(`https://restcountries.eu/rest/v2/alpha/${id}`);
+  const country = await res.json();
+
+  return country;
+};
+
+const Country = ({ country }) => {
+  const [border, setBorder] = useState([]);
+
+  const getBorders = async () => {
+    const borders = await Promise.all(
+      country.borders.map((border) => getCountry(border))
+    );
+
+    setBorder(borders);
+  };
+
+  useEffect(() => {
+    getBorders();
+  }, []);
+
+  console.log(border);
+
+  return (
+    <Layout title={country.name}>
+      <div className={styles.container}>
+        <div className={styles.container_left}>
+          <div className={styles.overview_panel}>
+            <img src={country.flag} alt={country.name} />
+
+            <h1 className={styles.overview_name}>{country.name}</h1>
+            <div className={styles.overview_region}>{country.region}</div>
+
+            <div className={styles.overview_numbers}>
+              <div className={styles.overview_population}>
+                <div className={styles.overview_value}>
+                  {country.population}
+                </div>
+                <div className={styles.overview_label}>Population</div>
+              </div>
+
+              <div className={styles.overview_area}>
+                <div className={styles.overview_value}>{country.area}</div>
+                <div className={styles.overview_label}>Area</div>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div className={styles.container_right}>
+          <div className={styles.details_panel}>
+            <h4 className={styles.details_panel_heading}>Details</h4>
+
+            <div className={styles.details_panel_row}>
+              <div className={styles.details_panel_label}>Capital</div>
+              <div className={styles.details_panel_capital}>
+                {country.capital}
+              </div>
+            </div>
+
+            <div className={styles.details_panel_row}>
+              <div className={styles.details_panel_label}>Language</div>
+              <div className={styles.details_panel_capital}>
+                {country.languages.map(({ name }) => name).join(", ")}
+              </div>
+            </div>
+
+            <div className={styles.details_panel_row}>
+              <div className={styles.details_panel_label}>Currency</div>
+              <div className={styles.details_panel_capital}>
+                {country.currencies.map(({ name }) => name).join(", ")}
+              </div>
+            </div>
+
+            <div className={styles.details_panel_row}>
+              <div className={styles.details_panel_label}>Native Name</div>
+              <div className={styles.details_panel_capital}>
+                {country.nativeName}
+              </div>
+            </div>
+
+            <div className={styles.details_panel_row}>
+              <div className={styles.details_panel_label}>Gini</div>
+              <div className={styles.details_panel_capital}>
+                {country.gini} %
+              </div>
+            </div>
+
+            <div className={styles.details_panel_borders}>
+              <div className={styles.details_panel_borders_label}>
+                Neighbor Countries
+              </div>
+
+              <div className={styles.details_panel_borders_container}>
+                {border.map((country) => (
+                  <div className={styles.details_panel_borders_country}>
+                    <img src={country.flag} alt={country.name} />
+                    <div className={styles.details_panel_borders_name}>
+                      {country.name}
+                    </div>
+                  </div>
+                ))}
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </Layout>
+  );
+};
+
+export default Country;
+
+export const getServerSideProps = async ({ params }) => {
+  const country = await getCountry(params.id);
+
+  return {
+    props: {
+      country,
+    },
+  };
+};
---
+++ src/pages/index.js
@@ -0,0 +1,52 @@
+import { useState } from "react";
+import CountriesTable from "../components/CountriesTable/CountriesTable";
+import Layout from "../components/Layout/Layout";
+import SearchInput from "../components/SearchInput/SearchInput";
+
+import styles from "../styles/Home.module.css";
+
+export default function Home({ countries }) {
+  const [keyword, setKeyword] = useState("");
+
+  const filteredCountries = countries.filter(
+    (country) =>
+      country.name.toLowerCase().includes(keyword) ||
+      country.region.toLowerCase().includes(keyword) ||
+      country.subregion.toLowerCase().includes(keyword) ||
+      country.capital.toLowerCase().includes(keyword) ||
+      country.nativeName.toLowerCase().includes(keyword)
+  );
+
+  const handleInputChange = (e) => {
+    e.preventDefault();
+    setKeyword(e.target.value.toLowerCase());
+  };
+
+  return (
+    <Layout>
+      <div className={styles.inputContainer}>
+        <div className={styles.counts}>Found {countries.length} countries</div>
+
+        <div className={styles.input}>
+          <SearchInput
+            placeholder="Filter by Name, Region or SubRegion"
+            onChange={handleInputChange}
+          />
+        </div>
+      </div>
+
+      <CountriesTable countries={filteredCountries} />
+    </Layout>
+  );
+}
+
+export const getStaticProps = async (ctx) => {
+  const res = await fetch("https://restcountries.eu/rest/v2/all");
+  const countries = await res.json();
+
+  return {
+    props: {
+      countries,
+    },
+  };
+};
